cfc7a1da82f1cab8b0b93194aeb509e605964811
saidul-islam-tuhin <tuhinmd718@gmail.com>
2019-08-31 17:40:02 +0600

    Custom controller

--- frappe-bench/apps/library_management/library_management/library_management/doctype/library_transaction/library_transaction.py
+++ frappe-bench/apps/library_management/library_management/library_management/doctype/library_transaction/library_transaction.py
@@ -3,8 +3,24 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class LibraryTransaction(Document):
-	pass
+    def validate(self):
+        last_transaction = frappe.get_list("Library Transaction",
+            fields=["transaction_type", "transaction_date"],
+            filters = {
+                "article": self.article,
+                "transaction_date": ("<=", self.transaction_date),
+                "name": ("!=", self.name)
+            })
+        if self.transaction_type=="Issue":
+            msg = _("Article {0} {1} has not been recorded as returned since {2}")
+            if last_transaction and last_transaction[0].transaction_type=="Issue":
+                frappe.throw(msg.format(self.article, self.article_name,
+                    last_transaction[0].transaction_date))
+        else:
+            if not last_transaction or last_transaction[0].transaction_type!="Issue":
+                frappe.throw(_("Cannot return article not issued"))
